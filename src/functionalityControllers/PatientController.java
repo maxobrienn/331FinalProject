5f9dfb3ce75077c997feec570a318730bbb6bf93
mderosa001 <mderosa001@csbsju.edu>
2024-04-26 15:32:18 -0500

    Small changes to patient controller

---
+++ src/functionalityControllers/PatientController.java
@@ -0,0 +1,45 @@
+package functionalityControllers;
+
+import tableConstructors.Patient;
+
+public class PatientController {
+	
+	//TODO 
+	public static void login(String username, String password) {
+		
+	}
+	
+	//TODO
+	public static Patient newPatient() {
+		return null;
+		
+	}
+	
+	//TODO
+	public static Patient viewPatient(String patientId) {
+		return null;
+		
+	}
+	
+	//TODO
+	public static void editPatientInfo(String patientId) {
+		
+	}
+	
+	//TODO
+	public static void viewAppointmentInfo(String patientId) {
+		
+	}
+	
+	//TODO
+	public static void viewDiagnosis(String paatientId) {
+		
+	}
+	
+	//TODO
+	public static void viewPrescriptions(String patientId) {
+		
+	}
+	
+	
+}
