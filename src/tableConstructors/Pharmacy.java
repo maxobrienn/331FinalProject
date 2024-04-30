9ad88be57d2bc6e187f95474862a7625a04612ed
mderosa001 <mderosa001@csbsju.edu>
2024-04-30 13:18:44 -0500

    update, view, and log in functionalities added to pharamcy

--- src/tableConstructors/Pharmacy.java
+++ src/tableConstructors/Pharmacy.java
@@ -1,7 +1,7 @@
 package tableConstructors;
-/**
- * @author Matt DeROsa
- */
+
+import java.sql.*;
+
 public class Pharmacy {
     private String pharmacyId;
     private String pharmacyName;
@@ -12,6 +12,8 @@ public class Pharmacy {
     private String phoneNumber;
     private String password;
     private String email;
+    
+    private boolean loggedIn = false;
 
     public Pharmacy() {
     }
@@ -28,132 +30,179 @@ public class Pharmacy {
         this.email = email;
     }
 
-	/**
-	 * @return the pharmacyId
-	 */
-	public String getPharmacyId() {
-		return pharmacyId;
-	}
+    public String getPharmacyId() {
+        return pharmacyId;
+    }
 
-	/**
-	 * @param pharmacyId the pharmacyId to set
-	 */
-	public void setPharmacyId(String pharmacyId) {
-		this.pharmacyId = pharmacyId;
-	}
+    public void setPharmacyId(String pharmacyId) {
+        this.pharmacyId = pharmacyId;
+    }
 
-	/**
-	 * @return the pharmacyName
-	 */
-	public String getPharmacyName() {
-		return pharmacyName;
-	}
+    public String getPharmacyName() {
+        return pharmacyName;
+    }
 
-	/**
-	 * @param pharmacyName the pharmacyName to set
-	 */
-	public void setPharmacyName(String pharmacyName) {
-		this.pharmacyName = pharmacyName;
-	}
+    public void setPharmacyName(String pharmacyName) {
+        this.pharmacyName = pharmacyName;
+    }
 
-	/**
-	 * @return the street
-	 */
-	public String getStreet() {
-		return street;
-	}
+    public String getStreet() {
+        return street;
+    }
 
-	/**
-	 * @param street the street to set
-	 */
-	public void setStreet(String street) {
-		this.street = street;
-	}
+    public void setStreet(String street) {
+        this.street = street;
+    }
 
-	/**
-	 * @return the city
-	 */
-	public String getCity() {
-		return city;
-	}
+    public String getCity() {
+        return city;
+    }
 
-	/**
-	 * @param city the city to set
-	 */
-	public void setCity(String city) {
-		this.city = city;
-	}
+    public void setCity(String city) {
+        this.city = city;
+    }
 
-	/**
-	 * @return the state
-	 */
-	public String getState() {
-		return state;
-	}
+    public String getState() {
+        return state;
+    }
 
-	/**
-	 * @param state the state to set
-	 */
-	public void setState(String state) {
-		this.state = state;
-	}
+    public void setState(String state) {
+        this.state = state;
+    }
 
-	/**
-	 * @return the zipCode
-	 */
-	public String getZipCode() {
-		return zipCode;
-	}
+    public String getZipCode() {
+        return zipCode;
+    }
 
-	/**
-	 * @param zipCode the zipCode to set
-	 */
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 
-	/**
-	 * @return the phoneNumber
-	 */
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
-	/**
-	 * @param phoneNumber the phoneNumber to set
-	 */
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	/**
-	 * @param password the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    // Getters and setters
-    
+    public Connection openDBConnection() {
+        try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            Connection myConnection = DriverManager.getConnection("jdbc:oracle:thin:@//cscioraclerh7srv.ad.csbsju.edu:1521/" +
+                    "csci.cscioraclerh7srv.ad.csbsju.edu","TEAM05", "TEAM05");
+            return myConnection;
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
+        return null;
+    }
+
+    public boolean login(String email, String password) {
+        Connection con = openDBConnection();
+        try {
+            PreparedStatement statement = con.prepareStatement("SELECT COUNT(*) FROM HEALTHCAREMANAGEMENT_PHARMACY WHERE EMAIL = ? AND PASSWORD = ?");
+            statement.setString(1, email);
+            statement.setString(2, password);
+
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next() && rs.getString(1) != null) {
+                this.loggedIn = true;
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void updatePharmacyInfo(String pharmacyId, String pharmacyName, String street, String city, String state, String zipCode, String phoneNumber, String password, String email) {
+        try {
+            Connection connection = openDBConnection();
+            CallableStatement callableStatement = connection.prepareCall("{call Edit_Pharmacy_Info(?,?,?,?,?,?,?,?,?)}");
+            callableStatement.setString(1, pharmacyId);
+            callableStatement.setString(2, phoneNumber);
+            callableStatement.setString(3, email);
+            callableStatement.setString(4, street);
+            callableStatement.setString(5, city);
+            callableStatement.setString(6, state);
+            callableStatement.setString(7, zipCode);
+            callableStatement.setString(8, password);
+            callableStatement.setString(9, email);
+
+            callableStatement.execute();
+
+            System.out.println("Pharmacy information updated successfully.");
+
+            callableStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Pharmacy displayPharmacyInfo(String pharmacyId) {
+        Pharmacy pharmacy = new Pharmacy();
+        Connection con = openDBConnection();
+        try {
+            String sql = "SELECT * FROM HealthCareManagement_PHARMACY WHERE PHARMACY_ID = ?";
+            PreparedStatement preparedStatement = con.prepareStatement(sql);
+            preparedStatement.setString(1, pharmacyId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                pharmacy.setPharmacyId(resultSet.getString("PHARMACY_ID"));
+                pharmacy.setPhoneNumber(resultSet.getString("PHONE_NUMBER"));
+                pharmacy.setEmail(resultSet.getString("EMAIL"));
+                pharmacy.setStreet(resultSet.getString("STREET"));
+                pharmacy.setCity(resultSet.getString("CITY"));
+                pharmacy.setState(resultSet.getString("STATE"));
+                pharmacy.setZipCode(resultSet.getString("ZIP_CODE"));
+                pharmacy.setPassword(resultSet.getString("PASSWORD"));
+                pharmacy.setEmail(resultSet.getString("EMAIL"));
+
+                System.out.println("Pharmacy ID: " + pharmacy.getPharmacyId());
+                System.out.println("Phone Number: " + pharmacy.getPhoneNumber());
+                System.out.println("Email: " + pharmacy.getEmail());
+                System.out.println("Street: " + pharmacy.getStreet());
+                System.out.println("City: " + pharmacy.getCity());
+                System.out.println("State: " + pharmacy.getState());
+                System.out.println("Zip Code: " + pharmacy.getZipCode());
+                System.out.println("Password: " + pharmacy.getPassword());
+                System.out.println("Email: " + pharmacy.getEmail());
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return pharmacy;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
 }
